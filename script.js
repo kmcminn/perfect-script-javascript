cb10f85563840f8607ff5140222871782a702ae1
kmcminn <tenyou@gmail.om>
2014-09-12 09:26:33 -0700

    moved agv checks and require into main, add semis, etc

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 #!/usr/bin/env js
 // script.js - a simple javascript script template
-//
-// no # for comments :/
-// no argument parsing library in stdlib :/
-// libraries are mostly async :/
+// author info, date
 
-// require the exec function from child_process
-process.exec = require('child_process').exec
 
 // usage function
 var usage = function() {
@@ -14,23 +9,17 @@ var usage = function() {
     console.log('  Usage: ' + process.argv[1] + ': <path> ');
     console.log('  <path>      full path to a directory');
     console.log();
-}
-
-// spew usage if we didnt get a path or got -h or --help
-if (process.argv.length < 3 || process.argv[2] === '-h' || process.argv[2] === '--help') {
-    usage();
-    process.exit(1);
-}
+};
 
 // simple async workaround, make callbacks trigger the end of the script
 var _scriptEnd = function(results, code) {
     console.log(results.stdout);
     process.exit(code);
-}
+};
 
 // our callback
 var _readFile = function(error, stdout, stderr) {
-    results = {};
+    var results = {};
     results.stdout = stdout;
     results.stderr = stderr;
     results.error = null;
@@ -39,13 +28,22 @@ var _readFile = function(error, stdout, stderr) {
         results.error = error
     }
     _scriptEnd(results, results.code)
-}
+};
 
 // hopefully someone will appreciate a main function
 var main = function() {
-    var path = process.argv[2];
-    var result = process.exec('ls -l ' + path, _readFile);
-}
+    // require the exec function from child_process
+    var exec = require('child_process').exec;
+    var argv = process.argv;
+
+    // spew usage if we didnt get a path or got -h or --help
+    if (argv.length < 3 || argv[2] === '-h' || argv[2] === '--help') {
+        usage();
+        process.exit(1);
+    }
+
+    exec('ls -l ' + argv[2], _readFile);
+};
 
 // only run if run directly instead of via require
-if (module.parent === null) main()
+if (module.parent === null) main();
